3f979af5d8f896a20110f7ede6b92984e1651d2f
Aleksei Voronov <despawn@gmail.com>
2023-11-29 10:45:47 +0100

    Add a way to force profile country by DID
    This is useful to fixup broken profiles.
    
    Ideally, of course, we should be retrying a few times and then giving up,
    but that seems kinda too much for a little hobby project.
    
    Also, metrics would be nice, but, you know

--- src/bin/force_profile_country.rs
+++ src/bin/force_profile_country.rs
@@ -2,7 +2,7 @@ extern crate nederlandskie;
 
 use std::env;
 
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use clap::Parser;
 use dotenv::dotenv;
 
@@ -11,8 +11,12 @@ use nederlandskie::services::{Bluesky, Database};
 #[derive(Parser, Debug)]
 struct Args {
     /// Handles of the users to force the country for, comma-separated
-    #[arg(long, required(true), value_delimiter(','))]
+    #[arg(long, value_delimiter(','))]
     handle: Vec<String>,
+
+    /// DIDs of the users to force the country for, comma-separated
+    #[arg(long, value_delimiter(','))]
+    did: Vec<String>,
 
     /// Country to use, two letters
     #[arg(long)]
@@ -24,6 +28,10 @@ async fn main() -> Result<()> {
     dotenv()?;
 
     let args = Args::parse();
+
+    if args.handle.is_empty() && args.did.is_empty() {
+        bail!("Either --handle or --did must be supplied");
+    }
 
     let database_url =
         env::var("DATABASE_URL").context("DATABASE_URL environment variable must be set")?;
@@ -39,6 +47,15 @@ async fn main() -> Result<()> {
 
         println!("Resolved handle '{}' to did '{}'", handle, did);
 
+        database.force_profile_country(&did, &args.country).await?;
+
+        println!(
+            "Stored '{}' as the country for profile with did '{}'",
+            args.country, did
+        );
+    }
+
+    for did in &args.did {
         database.force_profile_country(&did, &args.country).await?;
 
         println!(
